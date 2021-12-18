823a793eff94206a3fb929e0e5f3a00287eaa4c1
_chitiz <thenhernandezb@gmail.com>
2021-12-18 06:52:07 -0400

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import entriPoints from "./controller/api";
+import entriPoints from "./controller/api"; 
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (_req, res) => {
 	res.send(`dasfas`);
