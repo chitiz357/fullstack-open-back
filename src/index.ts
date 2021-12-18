0acc28daef46f33d0646ad1b3c0eb8cc985b86b1
_chitiz <thenhernandezb@gmail.com>
2021-12-18 07:11:50 -0400

    process env PORT

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import express from "express";
-import entriPoints from "./controller/api"; 
-import cors from 'cors';
+import entriPoints from "./controller/api";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.get("/", (_req, res) => {
 	res.send(`dasfas`);
 });
 
-entriPoints(app)
+entriPoints(app);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Sever is running on port ${PORT}`);
 });
