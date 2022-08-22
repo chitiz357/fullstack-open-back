87d32b94c57e2ca5b7049ba76049cfc20fd4f84d
bryan <thenhernandezb@gmail.com>
2022-08-22 18:58:52 -0400

    asdsdasd

--- src/notes/connection.ts
+++ src/notes/connection.ts
@@ -1,32 +1,32 @@
-import { Request, Response } from 'express';
-import * as fn from './function';
-import { Note } from './model';
+import { Request, Response } from 'express'
+import * as fn from './function'
+import { Note } from './model'
 
 export const getAllNotes = (_req: Request, res: Response) => {
-	const notes = fn.allNotes();
+	const notes = fn.allNotes()
 
-	res.send(notes);
-};
+	res.send(notes)
+}
 
 export const getOneNote = (req: Request, res: Response) => {
-	const id = Number(req.params.id);
-	const note = fn.oneNote(id);
+	const id = Number(req.params.id)
+	const note = fn.oneNote(id)
 
-	res.send(note);
-};
+	res.send(note)
+}
 
 export const postNote = (req: Request, res: Response) => {
-	const body = req.body;
+	const body = req.body
 
-	const tittle: string = body.title;
-	const content: string = body.content;
-	const note = new Note({ tittle, content });
-	fn.addNote(note);
-	res.send(note);
-};
+	const tittle: string = body.title
+	const content: string = body.content
+	const note: Note = fn.newNote(tittle, content)
+	fn.addNote(note)
+	res.send(note)
+}
 
 export const deleteNote = (req: Request, _res: Response) => {
 	const id = Number(req.params.id)
-	fn.removeNote(id);
+	fn.removeNote(id)
 	_res.send(fn.allNotes())
-};
+}
